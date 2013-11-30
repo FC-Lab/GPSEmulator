3ca7babe1bd429ddbc1cc6357d4e630df27e1831
falconh <wongfalcon@gmail.com>
2013-12-01 01:41:29 +0800

    Firebase added to collect the data from GPSEmulator. BackgroundLocationService and Constants added but not working properly.

--- src/com/example/gpsemulator/MainActivity.java
+++ src/com/example/gpsemulator/MainActivity.java
@@ -8,17 +8,47 @@ import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
+import com.firebase.client.* ;
+import java.util.Map ;
 
 public class MainActivity extends Activity {
 	
 	Button btnShowLocation ;
 	
 	GPSTracker gps ;
+	
+	Firebase falconhRef ;
+	
+	BackgroundLocationService currentLocation ;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		currentLocation = new BackgroundLocationService() ;
+		
+		falconhRef = new Firebase("https://falconh.firebaseio.com") ;
+		
+		falconhRef.addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange( DataSnapshot snapshot )
+			{
+				Object value = snapshot.getValue();
+				if ( value != null )
+				{
+					String lat = (String)((Map)value).get("Latitude");
+					String longi = (String)((Map)value).get("Longitude");
+					Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + lat + "\nLong: " + longi, Toast.LENGTH_LONG).show();
+				}
+			}
+
+			@Override
+			public void onCancelled(FirebaseError arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
 		btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
         
@@ -37,7 +67,11 @@ public class MainActivity extends Activity {
                     double longitude = gps.getLongitude();
                      
                     // \n is for new line
-                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();    
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show(); 
+                    
+                    falconhRef.child("Latitude").setValue(String.valueOf(latitude)) ;
+                    falconhRef.child("Longitude").setValue(String.valueOf(longitude));
+                    
                 }else{
                     // can't get location
                     // GPS or Network is not enabled
